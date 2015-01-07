12c70b98e98847685c0901432110a1a4f2571300
Dennis Taylor <dennis.taylor@goclio.com>
2015-01-07 12:47:39 -0800

    Add permissions action to /users endpoint.

--- lib/clio_client/models/user.rb
+++ lib/clio_client/models/user.rb
@@ -1,14 +1,14 @@
 module ClioClient
 
   class User < Resource
-    
+
     set_attributes(id:                {type: :int, readonly: true},
                    created_at:        {type: :datetime, readonly: true},
                    updated_at:        {type: :datetime, readonly: true},
                    subscription_plan: {type: :string, readonly: true},
                    time_zone:         {type: :string, readonly: true},
                    first_name:        {type: :string, readonly: true},
-                   last_name:         {type: :string, readonly: true}, 
+                   last_name:         {type: :string, readonly: true},
                    email:             {type: :string, readonly: true},
                    enabled:           {type: :boolean, readonly: true},
                    name:              {type: :string, readonly: true},
@@ -17,11 +17,15 @@ module ClioClient
                    co_counsel:        {type: :boolean, readonly: true}
                    )
 
-    has_association(:contact,            ClioClient::Contact, 
+    has_association(:contact,            ClioClient::Contact,
                     :polymorphic => true, :accepted_types => %w(Person Company))
 
     def avatar
       api.avatar(id)
+    end
+
+    def permissions
+      api.permissions(id)
     end
 
     private
