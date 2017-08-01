22621a0c3db4b788c5b25fc1f38c3b6ed24cfc86
Artem Hushcha <artem.gooscha@gmail.com>
2017-08-01 17:34:38 +0300

    Add field time_estimated to Task resource

--- lib/clio_client/models/task.rb
+++ lib/clio_client/models/task.rb
@@ -1,16 +1,17 @@
 module ClioClient
   class Task < Resource
 
-    set_attributes(id:            {type: :int, readonly: true},
-                   created_at:    {type: :datetime, readonly: true},
-                   updated_at:    {type: :datetime, readonly: true},
-                   name:          {type: :string},
-                   description:   {type: :string},
-                   priority:      {type: :string},
-                   due_at:        {type: :date},
-                   complete:      {type: :boolean},
-                   complete_at:   {type: :date},
-                   is_private:    {type: :boolean},
+    set_attributes(id:             {type: :int, readonly: true},
+                   created_at:     {type: :datetime, readonly: true},
+                   updated_at:     {type: :datetime, readonly: true},
+                   name:           {type: :string},
+                   description:    {type: :string},
+                   priority:       {type: :string},
+                   time_estimated: {type: :int},
+                   due_at:         {type: :date},
+                   complete:       {type: :boolean},
+                   complete_at:    {type: :date},
+                   is_private:     {type: :boolean},
                    is_statute_of_limitations:   {type: :boolean}
                    )
 
@@ -18,12 +19,12 @@ module ClioClient
     has_association :assigner, ClioClient::User
     has_association :matter, ClioClient::Matter
     has_many_association :reminders, ClioClient::Reminder
-    
+
 
     private
     def api
       session.tasks
     end
-      
+
   end
 end
